58226ba395821998502431e371ffe1d879cd1b15
Vasyan <vasyan.coder@gmail.com>
2023-07-07 22:03:09 +0300

    feat: added root screen

--- lib/app/ui/main_app_builder.dart
+++ lib/app/ui/main_app_builder.dart
@@ -1,5 +1,6 @@
 import 'package:client_it/app/di/init_di.dart';
 import 'package:client_it/app/domain/app_builder.dart';
+import 'package:client_it/app/ui/root_screen.dart';
 import 'package:client_it/feature/auth/domain/auth_repository.dart';
 import 'package:client_it/feature/auth/domain/auth_state/auth_cubit.dart';
 import 'package:flutter/material.dart';
@@ -12,7 +13,7 @@ class MainAppBuilder implements AppBuilder {
       child: MaterialApp(
         home: Scaffold(
           body: Center(
-            child: Text("Hello"),
+            child: RootScreen(),
           ),
         ),
       ),
---
+++ lib/feature/auth/ui/login_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      title: const Text("LoginScreen"),
+    ));
+  }
+}
