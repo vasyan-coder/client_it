146564821fa26384f6f3377ac67ccc46ed1e5606
Vasyan <vasyan.coder@gmail.com>
2023-07-07 22:37:21 +0300

    feat: added login screen

--- lib/app/ui/root_screen.dart
+++ lib/app/ui/root_screen.dart
@@ -10,7 +10,7 @@ class RootScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AuthBuilder(
-      isNotAuthorized: (context) => const LoginScreen(),
+      isNotAuthorized: (context) => LoginScreen(),
       isWaiting: (context) => const AppLoader(),
       isAuthorized: (context, value, child) => MainScreen(userEntity: value),
     );
--- lib/feature/auth/ui/login_screen.dart
+++ lib/feature/auth/ui/login_screen.dart
@@ -1,14 +1,58 @@
-import 'package:flutter/cupertino.dart';
+import 'package:client_it/app/ui/components/app_text_button.dart';
+import 'package:client_it/app/ui/components/app_text_field.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  LoginScreen({super.key});
+
+  final controllerLogin = TextEditingController();
+  final controllerPassword = TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: const Text("LoginScreen"),
-    ));
+      appBar: AppBar(title: const Text("LoginScreen")),
+      body: Form(
+        key: formKey,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 30, right: 30),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                AppTextField(
+                  controller: controllerLogin,
+                  labelText: 'Login',
+                ),
+                const SizedBox(height: 16),
+                AppTextField(
+                  controller: controllerPassword,
+                  labelText: 'Password',
+                  obscureText: true,
+                ),
+                const SizedBox(height: 16),
+                AppTextButton(
+                  onPressed: () {
+                    if (formKey.currentState?.validate() == true) {
+                      print("pressed login");
+                    }
+                  },
+                  text: 'Sign In',
+                ),
+                AppTextButton(
+                  onPressed: () {
+                    if (formKey.currentState?.validate() == true) {
+                      print("pressed registration");
+                    }
+                  },
+                  text: 'Sign Up',
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
