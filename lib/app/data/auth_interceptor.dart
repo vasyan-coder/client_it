ac25fe412fde7aaae09576e3f5c3f45162df5046
Vasyan <vasyan.coder@gmail.com>
2023-07-08 21:32:28 +0300

    feat: added 401 error Interception

--- lib/app/data/auth_interceptor.dart
+++ lib/app/data/auth_interceptor.dart
@@ -1,3 +1,4 @@
+import 'package:client_it/app/data/dio_container.dart';
 import 'package:client_it/app/di/init_di.dart';
 import 'package:client_it/feature/auth/domain/auth_state/auth_cubit.dart';
 import 'package:dio/dio.dart';
@@ -15,6 +16,25 @@ class AuthInterceptor extends QueuedInterceptor {
       final headers = options.headers;
       headers["Authorization"] = "Bearer $accessToken";
       super.onRequest(options.copyWith(headers: headers), handler);
+    }
+  }
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) async {
+    if (err.response?.statusCode == 401) {
+      try {
+        await locator.get<AuthCubit>().refreshToken();
+        final request = await locator
+            .get<DioContainer>()
+            .dio
+            .request(err.requestOptions.path,
+        );
+        return handler.resolve(request);
+      } catch (_) {
+        super.onError(err, handler);
+      }
+    } else {
+      super.onError(err, handler);
     }
   }
 }
